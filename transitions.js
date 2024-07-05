2697523911fc18f8aeb7b7f77ab9f7c6ac873064
najeeb-yusuf <yusufnajlawal@gmail.com>
2024-07-05 19:37:19 +0100

    first commit

---
+++ transitions.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const elements = document.querySelectorAll('.transition-slide');
+
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('visible');
+            }
+        });
+    });
+
+    elements.forEach(element => {
+        observer.observe(element);
+    });
+});
